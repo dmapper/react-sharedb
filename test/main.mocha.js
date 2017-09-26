8bee42760f771c966ce70dd28b0c8b4f34816ab6
Pavel Zhukov <cray0000@gmail.com>
2017-09-26 19:13:02 +0300

    [tests] test parametrized. Add a bunch of helpers

--- test/main.mocha.js
+++ test/main.mocha.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { expect } from 'chai'
 import { mount } from 'enzyme'
 import { createWaitForElement } from 'enzyme-wait'
+import _ from 'lodash'
 import './_server'
 
 // import Simple from './stubs/Simple'
@@ -11,11 +12,56 @@ ReactWrapper.prototype.waitFor = function (selector) {
   return createWaitForElement(selector)(this)
 }
 
-describe('main', () => {
-  it('should test something', async () => {
-    const Simple = require('./stubs/Simple')
-    let w = mount(<Simple />)
-    await w.waitFor('.Simple')
-    expect(w.html()).to.have.string('test1').and.have.string('test2')
+let subscribe
+
+async function initSimple (subscribeParamsFn) {
+  let Simple = require('./stubs/Simple')
+  Simple = subscribe(subscribeParamsFn)(Simple)
+  let w = mount(<Simple />)
+  await w.waitFor('.Simple')
+  w.getItems = function () {
+    return getSimpleItems(this)
+  }
+  Object.defineProperty(w, 'items', {
+    get: function () { return this.getItems() }
+  })
+  return w
+}
+
+function getSimpleItems (w) {
+  return w.find('.Simple').text().split(',')
+}
+
+function alias (number) {
+  const name = n => `test${n}_`
+  if (_.isArray(number)) {
+    return number.map(n => name(n))
+  } else {
+    return name(number)
+  }
+}
+
+describe('Queries', () => {
+  // Workaround to init subscribe only after the server started (which is a global before)
+  before(() => {
+    subscribe = require('../src').subscribe
+  })
+
+  it('all collection', async () => {
+    let w = await initSimple(() => ({items: ['users', {}]}))
+    expect(w.items.length).to.eql(5)
+    expect(w.items).to.include.members(alias([1, 2, 3, 4, 5]))
+  })
+
+  it('parametrized 1', async () => {
+    let w = await initSimple(() => ({items: ['users', {color: 'blue'}]}))
+    expect(w.items.length).to.eql(2)
+    expect(w.items).to.include.members(alias([1, 2]))
+  })
+
+  it('parametrized 2', async () => {
+    let w = await initSimple(() => ({items: ['users', {color: 'red'}]}))
+    expect(w.items.length).to.eql(3)
+    expect(w.items).to.include.members(alias([3, 4, 5]))
   })
 })
--- test/stubs/Simple.jsx
+++ test/stubs/Simple.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import {subscribe} from '../../src'
 
-@subscribe(props => ({
-  users: ['users', {}]
-}))
-
 export default class Simple extends React.Component {
 
   constructor (props) {
@@ -13,8 +9,8 @@ export default class Simple extends React.Component {
   }
 
   render () {
-    let {users} = this.props
-    return <div className='Simple'>{ users.map(i => i.name).join(',') }</div>
+    let {items} = this.props
+    return <div className='Simple'>{ items.map(i => i.name).join(',') }</div>
   }
 
 }

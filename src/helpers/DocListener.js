1f40ea697298ac5e52c1e6ffa47e2c919472d6d2
Pavel Zhukov <cray0000@gmail.com>
2017-11-06 02:36:41 +0200

    Increase the max listeners of the shareDoc

--- src/helpers/DocListener.js
+++ src/helpers/DocListener.js
@@ -4,6 +4,7 @@ import model from '../model'
 
 // Updates to the following fields are going to be ignored (props WON'T be updated)
 const IGNORE_FIELDS = ['_meta', 'updatedAt', 'updatedBy']
+const MAX_LISTENERS = 100
 
 export default class DocListener extends EventEmitter {
   constructor (collection, docId) {
@@ -16,6 +17,7 @@ export default class DocListener extends EventEmitter {
     let { collection, docId } = this
     let shareDoc = model.connection.get(collection, docId)
     if (!shareDoc) return console.error('Doc not found:', collection, docId)
+    shareDoc.setMaxListeners(MAX_LISTENERS)
     this.listeners = []
     let fn = () => this.emit('update')
     ;['op', 'del', 'create'].forEach(eventName => {

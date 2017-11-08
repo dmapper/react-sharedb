ac3f32c1cc0e1f04ea7c0a489281c6ed6888538e
Pavel Zhukov <cray0000@gmail.com>
2017-11-08 06:34:14 +0200

    Add @local and @singleton decorators

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as model } from './model'
 export { default as subscribe } from './subscribe'
+export { default as singleton } from './singleton'
+export { default as local } from './local'
---
+++ src/singleton.jsx
@@ -0,0 +1,23 @@
+import model from './model'
+
+const DEFAULT_COLLECTION = '$components'
+
+export default function singleton (Component) {
+  let model = generateScopedModel()
+  return function (Component) {
+    Component.model = model
+    Component.path = model.path.bind(model)
+    let _componentWillUnmount = Component.prototype.componentWillUnmount
+    Component.prototype.componentWillUnmount = function (...args) {
+      if (_componentWillUnmount) _componentWillUnmount.call(this, ...args)
+      model.removeContextListeners()
+      model.destroy()
+    }
+    return Component
+  }
+}
+
+function generateScopedModel () {
+  let path = `${DEFAULT_COLLECTION}.${model.id()}`
+  return model.scope(path)
+}

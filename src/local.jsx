95d155612904e891a229e58ed6c0ed996a175098
Artur Zayats <zag2art@gmail.com>
2018-05-27 15:47:44 +0300

    add autorerender

--- src/local.jsx
+++ src/local.jsx
@@ -1,4 +1,5 @@
 import model from './model'
+import Tracker from 'trackerjs'
 
 const DEFAULT_COLLECTION = '$components'
 
@@ -6,6 +7,7 @@ export default function local (Component) {
   let _componentWillMount = Component.prototype.componentWillMount
   Component.prototype.componentWillMount = function (...args) {
     this.model = generateScopedModel()
+    this.autorunRender()
     if (_componentWillMount) _componentWillMount.call(this, ...args)
   }
   let _componentWillUnmount = Component.prototype.componentWillUnmount
@@ -13,6 +15,16 @@ export default function local (Component) {
     if (_componentWillUnmount) _componentWillUnmount.call(this, ...args)
     this.model.destroy()
     delete this.model
+  }
+  Component.prototype.autorunRender = function () {
+    let oldRender = this.render
+    this.render = () => {
+      // Simple method we can offer in the `Meteor.Component` API
+      return this.autorunOnce('_renderComputation', oldRender)
+    }
+  }
+  Component.prototype.autorunOnce = function (name, dataFunc) {
+    return Tracker.once(name, this, dataFunc, this.forceUpdate)
   }
   return Component
 }
